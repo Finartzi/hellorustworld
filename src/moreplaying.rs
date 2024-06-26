bb15c0880940f96678aa4279bd9fee6238bc39b2
artzi <arto.paeivinen@gmail.com>
2024-06-26 18:40:12 +0300

    Now playing a bit more with strings

--- src/moreplaying.rs
+++ src/moreplaying.rs
@@ -2,6 +2,8 @@ pub fn playmore() {
     boxing();
     tuples();
     mutable();
+    using_ref();
+    replace_part();
 }
 
 fn boxing() {
@@ -49,4 +51,48 @@ fn mutable() {
 
 fn change(somestring: &mut String) {
     somestring.push_str(", world again");
+}
+
+fn using_ref() {
+    let c: char = '有';
+    let r1: &char = &c;
+
+    let ref r2 = c;
+    assert_eq!(*r1, *r2);
+
+    // check the equality of the two address strings
+    assert_eq!(get_address(r1), get_address(r2));
+    println!("Success");
+
+    let s = String::from("Hello World");
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    print!("{}", hello);
+    println!("{}", world);
+
+    let t: &str = "Hello another world";
+    println!("{}", t);
+
+    let u: Box<str> = "Hello somewhat boxed".into();
+    greetings(&u);
+
+    let mut v: String = String::from("");
+    v.push_str("Hallo myself");
+    v.push('!');
+    println!("{}", v);
+}
+
+fn greetings(s: &str) {
+    println!("{}", s);
+}
+fn get_address(r: &char) -> String {
+    format!("{:p}", r)
+}
+
+fn replace_part() {
+    let s: String = String::from("I like dogs");
+    // allocate new memory and store the modified string there
+    let s1: String = s.replace("dogs", "cats");
+    println!("{}", s1);
 }
